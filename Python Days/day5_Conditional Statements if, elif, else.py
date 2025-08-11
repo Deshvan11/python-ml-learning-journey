c22e8befa0681558aa16eb5fcf8165b8d88cf513
Tanmay Deshmukh <tndeshmukh11@gmail.com>
2025-08-11 16:10:18 +0530

    Added one more Q.
    about while loop

--- Python Days/day5_Conditional Statements if, elif, else.py
+++ Python Days/day5_Conditional Statements if, elif, else.py
@@ -78,3 +78,34 @@ elif h>40:
     print((40*r)+(pay*(r*1.5)))
     
     
+#Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. 
+# Once 'done' is entered, print out the largest and smallest of the numbers. 
+# If the user enters anything other than a valid number catch it with a try/except and put out an 
+# appropriate message and ignore the number. Enter 7, 2, bob, 10, and 4 and match the output below.
+
+smallest = None
+largest = None
+
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        num = int(num)
+    except:
+        print("Invalid input")
+        continue
+    
+    if smallest is None or num < smallest:
+        smallest = num
+    if largest is None or num > largest:
+        largest = num
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
+#output:=
+#Invalid input
+#Maximum is 10
+#Minimum is 2
+    
+    
